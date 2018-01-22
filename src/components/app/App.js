ef3e2867b2fa475adfe11f0f22721c7f2c95339d
Jcastillo <Johnfra797@hotmail.com>
2018-01-22 14:40:56 -0500

    React Router Example

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import './App.css';
 import Hello from '../hello/Hello';
+import BasicExample from '../route/Route';
+
 class App extends Component {
   render() {
     return (
@@ -12,6 +14,7 @@ class App extends Component {
         </header>
         <Hello>
         </Hello>
+        <BasicExample></BasicExample>
       </div>
     );
   }
